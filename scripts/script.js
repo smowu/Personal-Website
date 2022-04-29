cbae445c04791dd614013afb9bf1b46147176ca2
smowu <smowu@users.noreply.github.com>
2022-04-30 00:18:41 +0800

    Smol changes

--- scripts/script.js
+++ scripts/script.js
@@ -23,6 +23,7 @@ function removeBlur()
   //navStyle.classList.toggle('navbar-no-blur');
   navbar.style.boxShadow = '0px 0px 0px rgba(0, 0, 0,0)';
 }
+
 /*
 window.onload = function(){
   document.querySelector('.btn').onclick = function(){
