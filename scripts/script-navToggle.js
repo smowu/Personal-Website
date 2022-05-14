6ae8b2d7ce3f25900956e003f41280f4cd12851c
smowu <smowu@users.noreply.github.com>
2022-05-14 16:41:56 +0800

    Update

--- scripts/script-navToggle.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector("#navToggle");
-const nav = document.querySelector("#nav-links");
-
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle('nav-open')
-});
